6e271dc8a6f567f8d40a4eb8d8b3379d4c1641c0
sandoro <osanyi85@gmail.com>
2022-03-02 13:03:31 +0100

    Fetch is working

--- reactinitial/src/App.js
+++ reactinitial/src/App.js
@@ -1,7 +1,24 @@
 import React, { useEffect, useState } from "react"
+import http from "axios"
+import LoadingMask from "./components/LoadingMask"
 
 const App = () => {
+  const [laptops, setLaptops] = useState(null);
+  const [isShown, setIsShown] = useState(false);
 
+  const load = async() => {
+    const response = await http.get("https://demoapi.com/api/laptop")
+    // console.group(response.data);
+    setLaptops(response.data);
+  }
+
+useEffect(() => {
+  load();
+  setTimeout(() => {
+    setIsShown(true)
+  }, 2 * 1000);
+
+}, []);
   return (
     <div>
       Todo...
---
+++ reactinitial/src/components/LoadingMask.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LoadingMask = () => {
+return (
+    <div>Loading...</div>
+)
+
+}
+
+export default LoadingMask
